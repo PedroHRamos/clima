11f7f69f854c862416d5421eb1502f1a6265fee7
Pedro Henrique Ramos <pedrohramos.ti@gmail.com>
2022-01-24 18:17:11 -0300

    Fixing layout and creating DTO

--- clima/src/app/services/servico.service.ts
+++ clima/src/app/services/servico.service.ts
@@ -1,12 +1,33 @@
 import { Injectable } from '@angular/core';
+const weatherAPIKey = require('../../../APIKey.json').Key;
 
-@Injectable({
+@Injectable({ 
   providedIn: 'root'
 })
 export class ServicoService {
 
   constructor() { }
 
+  /*
+
+    POSSÍVEIS CHAMADAS
   
+    api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+    api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
+
+    api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
+
+    &lang=pt_br - Tradução pra ptbr
+    &units=metric - Conversão para metros
+    
+    Exemplo:
+    https://api.openweathermap.org/data/2.5/find?q=Belo%20Horizonte&lang=pt_br&units=metric&appid=APIKey
+
+  */
+
+    
+
+
 
 }
